6719e7302f28221eb31bd59656eab04bd5a76276
SalahuddinShayan <104673244+SalahuddinShayan@users.noreply.github.com>
2024-10-26 17:11:24 +0530

    V-1.04

--- src/main/java/com/wordsmith/WordsmithApplication.java
+++ src/main/java/com/wordsmith/WordsmithApplication.java
@@ -2,8 +2,13 @@ package com.wordsmith;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.github.valb3r.letsencrypthelper.tomcat.TomcatWellKnownLetsEncryptChallengeEndpointConfig;
+
 
 @SpringBootApplication
+@Import(TomcatWellKnownLetsEncryptChallengeEndpointConfig.class)
 public class WordsmithApplication {
 
 	public static void main(String[] args) {
