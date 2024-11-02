083d13f2e38e8b44c95e099387a2bc570ef9f3d6
SalahuddinShayan <104673244+SalahuddinShayan@users.noreply.github.com>
2024-11-02 15:37:17 +0530

    V-1.065

--- src/main/java/com/wordsmith/Repositories/ChapterRepository.java
+++ src/main/java/com/wordsmith/Repositories/ChapterRepository.java
@@ -18,16 +18,16 @@ public interface ChapterRepository extends JpaRepository<Chapter, Long> {
 	@Query(value = "SELECT chapter_id FROM chapter WHERE novel_name = :novelname and chapter_no = :chapterno", nativeQuery = true)
     long  findChapterId(@Param("novelname") String novelname, @Param("chapterno") String chapterno);
 	
-	@Query(value = "SELECT chapter_id FROM chapter WHERE novel_name = :novelname AND chapter_id = (select min(chapter_id) from chapter where chapter_id > :chapterid)", nativeQuery = true)
+	@Query(value = "select min(chapter_id) from chapter where novel_name = :novelname and chapter_id > :chapterid;", nativeQuery = true)
     long  NextChapterId(@Param("novelname") String novelname, @Param("chapterid") long chapterid);
 	
-	@Query(value = "SELECT chapter_id FROM chapter WHERE novel_name = :novelname AND chapter_id = (select max(chapter_id) from chapter where chapter_id < :chapterid)", nativeQuery = true)
+	@Query(value = "select max(chapter_id) from chapter where novel_name = :novelname and chapter_id < :chapterid;", nativeQuery = true)
     long  PreviousChapterId(@Param("novelname") String novelname, @Param("chapterid") long chapterid);
 	
-	@Query(value = "SELECT * FROM chapter WHERE novel_name = :novelname order by chapter_no desc limit 3;", nativeQuery = true)
+	@Query(value = "SELECT * FROM chapter WHERE novel_name = :novelname order by chapter_id desc limit 3;", nativeQuery = true)
     List<Chapter>  Latest(@Param("novelname") String novelname);
 	
-	@Query(value = "SELECT * FROM chapter WHERE novel_name = :novelname order by chapter_no desc limit 1;", nativeQuery = true)
+	@Query(value = "SELECT * FROM chapter WHERE novel_name = :novelname order by chapter_id desc limit 1;", nativeQuery = true)
     Chapter  Latest1(@Param("novelname") String novelname);
 
 }
--- src/main/webapp/novel/noveltemplate.jsp
+++ src/main/webapp/novel/noveltemplate.jsp
@@ -100,7 +100,7 @@
          <div class="col-1 col-lg-3 py-3 "></div>
          </div>
         
-        
+        <div id="disqus_thread"></div>
         
         <footer class ="stm">
          <div  Style = "margin-top: 30px;" class = "row">
@@ -116,7 +116,24 @@
          </div>
         </footer>
        
-        
+<script>
+    /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+    
+    var disqus_config = function () {
+    this.page.url = "https://easternwordsmith.com/novel/${novel.novelName}";  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = "Novel_ID:${novel.novelId}"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://easternwordsmith.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>        
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" ></script>
 <script type="text/javascript" src="../js/script.js"></script>  
 </body>
